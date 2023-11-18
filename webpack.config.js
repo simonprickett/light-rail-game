35c96ca9b9e33c60ce1ba87bcc872240d610bb46
Simon Prickett <simon@crudworks.org>
2023-11-18 21:12:14 +0000

    Moved some assets to CDN.

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,13 @@ module.exports = {
   target: [ 'web' ],
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.ejs'
+      template: './src/index.ejs',
+      templateParameters: {
+
+      }
     }),
     new CopyPlugin({
       patterns: [
-//        { context: './src/', from: 'index.html', to: 'index.html' },
-        { context: './src/', from: 'js/fontawesome.js', to: 'js/' },
         { context: './src/', from: 'css', to: 'css' },
         { context: './src/', from: 'images', to: 'images' },
         { context: './src/', from: 'data', to: 'data' },
